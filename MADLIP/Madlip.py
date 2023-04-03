9237a643e0b0cd9437d3873a2726dced9d990713
Yohannesiv <Yohannesiv>
2023-04-03 10:48:44 +0300

    madlip

---
+++ MADLIP/Madlip.py
@@ -0,0 +1,5 @@
+name = input("name ")
+verb = input("verb ")
+name2 = input("name 2 ")
+
+madlip = print(f"It was pizza day at school, and {name} was super {verb} for lunch. But when she went outside to eat, a bird stole her pizza! {name2} chased the bird all over school. She climbed, jumped, and ran through the playground.")
